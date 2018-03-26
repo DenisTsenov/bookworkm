87f603512004d90a9b3561ccb8b16a21f5fc5eac
DenisTsenov <denis_cenov_@abv.bg>
2018-03-26 19:23:44 +0300

    statistics now  avaliable for  my acc

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 $now = time();
 
 if (isset($_SESSION['discard_after']) && $now > $_SESSION['discard_after']) {
-    // this session has worn out its welcome; kill it and start a brand new one
+    // the time  has expired and  start  new  session
     session_unset();
     session_destroy();
     session_start();
