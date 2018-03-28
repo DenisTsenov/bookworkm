f9c522d51a637cc9c2da6b4311964409964bebc2
DenisTsenov <denis_cenov_@abv.bg>
2018-03-29 01:30:48 +0300

    front page bug fix

--- index.php
+++ index.php
@@ -67,6 +67,8 @@ require_once __DIR__ . '/view/header.php';
                 header("Location: index.php");
             } elseif (isset($_GET["page"])) {
                 require_once "./view/" . $_GET["page"] . ".php";
+            }else{
+                require_once "./view/main.php";
             }
 
 //            var_dump($_SESSION);
@@ -85,47 +87,7 @@ require_once __DIR__ . '/view/header.php';
     </div>
     <script src="./assets/js/ajax.js"></script>
     <script src="./assets/js/jQuery.js"></script>
-    <script type="text/javascript">
-                var request = new XMLHttpRequest();
-                request.open("GET", "./controller/likesController.php?liked=1", true);
-                request.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        var response = JSON.parse(this.responseText);
-                        var main = document.getElementById("baic_content");
-
-                        for (arr in response) {
-
-                            var console = document.createElement("DIV");
-                            console.setAttribute("class", "liked_book");
-
-                            var img = document.createElement("img");
-                            img.src = "./assets/uploads/product_img/" + response[arr]["img"];
-                            img.setAttribute("id", "liked_img");
-
-                            var likes = document.createElement("p");
-                            likes.innerHTML = "Likes: " + response[arr]["likes"];
-
-                            var name = document.createElement("p");
-                            name.innerHTML = "Name: " + response[arr]["name"];
-
-                            var price = document.createElement("p");
-                            price.innerHTML = "Price: " + response[arr]["price"];
-
-                            var quantity = document.createElement("p");
-                            quantity.innerHTML = "Quantity: " + response[arr]["quantity"];
-
-                            console.appendChild(img);
-                            console.appendChild(likes);
-                            console.appendChild(name);
-                            console.appendChild(price);
-                            console.appendChild(quantity);
-                             main.appendChild(console);
-                        }
-                       
-                    }
-                };
-                request.send();
-    </script>
+    
     <?php
     include_once './view/footer.php';
     
